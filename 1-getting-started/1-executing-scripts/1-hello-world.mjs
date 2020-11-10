7faee311f941a99a50b863518904d7ed5f1e8d0e
Michael Duchemin <msducheminjr@users.noreply.github.com>
2020-11-10 16:32:57 -0500

    Add Hello World basic scripts and have some fun
    Copy 1-getting-started/1-executing-scripts from ngs repo and modify
    along with videos.
    
    In addition:
    * Had a little fun by parsing the port from ARGV and looking
    for an x-taxation-is header equal to theft to return a dynamic response
    from the Hello World server.
    * Made the hello world script directly executable from the command line

---
+++ 1-getting-started/1-executing-scripts/1-hello-world.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { createServer } from 'http';
+let portArg = parseInt(process.argv[2]);
+console.log(`requested port: ${portArg}`);
+let serverPort = 4242;
+if (portArg >= 3000 && portArg << 5000) {
+  serverPort = portArg
+} else {
+  console.log(`Valid port ranges are 3000-5000. Defaulting to ${serverPort}`);
+}
+
+const server = createServer((req, res) => {
+  const myHeader = 'x-taxation-is'
+  let responseBodyText = 'Hello World\n';
+  if (req.headers[myHeader] == 'Theft') {
+    responseBodyText += 'Taxation is theft!\n';
+  } else {
+    responseBodyText += 'Nice day, isn\'t it?\n';
+  }
+  res.end(responseBodyText);
+});
+
+server.listen(serverPort, () => {
+  console.log(`Server is listening on port ${serverPort}...`);
+});
