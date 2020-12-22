0223e806060ca1e9a59464780b06e89ed8de005b
Michael Duchemin <msducheminjr@users.noreply.github.com>
2020-12-22 16:08:57 -0500

    Add EventEmitter example

--- 4-modules/7-event-emitters/example.js
+++ 4-modules/7-event-emitters/example.js
@@ -2,3 +2,25 @@ const EventEmitter = require('events');
 
 // Streams are Event Emitters
 // process.stdin, process.stdout
+const myEmitter = new EventEmitter();
+
+setImmediate(() => {
+  myEmitter.emit('TAXATION_EVENT');
+});
+
+
+myEmitter.on('TAXATION_EVENT', () => {
+  console.log('Taxation has occurred.');
+});
+
+myEmitter.on('TAXATION_EVENT', () => {
+  console.log('Politically connected cronies get even fatter.');
+});
+
+myEmitter.on('TAXATION_EVENT', () => {
+  console.log('Large percentage of the money never leaves the beltway.');
+});
+
+myEmitter.on('TAXATION_EVENT', () => {
+  console.log('The Fed debases currency to cover the shortfall.');
+});
